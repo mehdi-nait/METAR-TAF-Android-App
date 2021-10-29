72840a705962ba66568623c29b9c80ef428fd237
mehdi-nait <mehdi.nait.hammou1@gmail.com>
2021-10-30 01:10:41 +0200

    add top nav bar

--- app/src/main/java/com/example/metar_taf/MainActivity2.java
+++ app/src/main/java/com/example/metar_taf/MainActivity2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
