7775330dccb385f331809d3b35e3a3f14d840084
mehdi-nait <mehdi.nait.hammou1@gmail.com>
2021-10-29 23:18:02 +0200

    adding top bar

--- app/src/main/java/com/example/metar_taf/MainActivity.java
+++ app/src/main/java/com/example/metar_taf/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.metar_taf;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +27,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar =  getSupportActionBar();
 
+        //change action bar color
+        String colorHex = "#" + Integer.toHexString(ContextCompat.getColor(getApplicationContext(), R.color.bar_color) & 0x00ffffff);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor(colorHex));
+        actionBar.setBackgroundDrawable(colorDrawable);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.top_bar,null);
+        actionBar.setCustomView(view);
 
         find_button = (Button) findViewById(R.id.find_button);
         code = (EditText) findViewById(R.id.OACI);
--- app/src/main/java/com/example/metar_taf/MainActivity2.java
+++ app/src/main/java/com/example/metar_taf/MainActivity2.java
@@ -1,8 +1,15 @@
 package com.example.metar_taf;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +21,17 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ActionBar actionBar =  getSupportActionBar();
+
+        //change action bar color
+        String colorHex = "#" + Integer.toHexString(ContextCompat.getColor(getApplicationContext(), R.color.bar_color) & 0x00ffffff);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor(colorHex));
+        actionBar.setBackgroundDrawable(colorDrawable);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.top_bar,null);
+        actionBar.setCustomView(view);
 
         code = findViewById(R.id.txt_code);
 
